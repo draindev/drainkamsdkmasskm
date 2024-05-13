00a79cf91e88f3e017f08b582e8de02433891559
draindev <goongoldigon@gmail.com>
2024-05-13 08:20:17 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -129,8 +129,8 @@ const Home: NextPage = () => {
           ...presaleContract,
           functionName: 'maxContribution'
         },
-
-
+	query:{
+        enabled: account.isConnected,
       }
     },
 
